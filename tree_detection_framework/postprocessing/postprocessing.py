4590ed7dafe0f610b35aa984449547afdd141b55
David Russell <djrussell@ucdavis.edu>
2024-10-07 20:56:19 +0000

    Add docstring for post-processing

--- tree_detection_framework/postprocessing/postprocessing.py
+++ tree_detection_framework/postprocessing/postprocessing.py
@@ -4,9 +4,29 @@ from tree_detection_framework.detection.region_detections import (
 )
 
 
-def single_region_NMS(detections: RegionDetections):
+def single_region_NMS(detections: RegionDetections) -> RegionDetections:
+    """Run non-max suppresion on predictions from a single region.
+
+    Args:
+        detections (RegionDetections): Detections from a single region to run NMS on.
+
+    Returns:
+        RegionDetections:
+            NMS-suppressed set of detections
+    """
     raise NotImplementedError()
 
 
-def multi_region_NMS(detections: RegionDetectionSet):
+def multi_region_NMS(detections: RegionDetectionsSet) -> RegionDetections:
+    """Run non-max suppresion on predictions from multiple regions.
+
+    Args:
+        detections (RegionDetectionsSet): Detections from multiple regions to run NMS on.
+
+    Returns:
+        RegionDetections:
+            NMS-suppressed set of detections, merged together for the set of regions.
+    """
+    # This may implement more sophisticated algorithms, such as down-weighting predictions at the
+    # boundaries or first performing within-tile NMS before across-tile NMS for computational reasons.
     raise NotImplementedError()
