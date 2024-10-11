c80b77ecf8a068c6c6cab26779a67a0a1d8f645c
Amritha <amrithasp02@gmail.com>
2024-10-11 19:07:14 +0000

    Refactor preprocessing steps, add classes to derived_geodatasets and add a utils file

---
+++ tree_detection_framework/preprocessing/derived_geodatasets.py
@@ -0,0 +1,144 @@
+from typing import Any
+
+import fiona
+import fiona.transform
+import numpy as np
+import rasterio
+import shapely.geometry
+from shapely.affinity import affine_transform
+import matplotlib.pyplot as plt
+
+from torchgeo.datasets import (
+    RasterDataset,
+    VectorDataset,
+)
+from torchgeo.datasets.utils import BoundingBox, array_to_tensor
+
+
+class CustomRasterDataset(RasterDataset):
+    """
+    Custom dataset class for orthomosaic raster images. This class extends the `RasterDataset` from `torchgeo`.
+
+    Attributes:
+        filename_glob (str): Glob pattern to match files in the directory.
+        is_image (bool): Indicates that the data being loaded is image data.
+        separate_files (bool): True if data is stored in a separate file for each band, else False.
+    """
+
+    filename_glob: str = "*.tif"  # To match all TIFF files
+    is_image: bool = True
+    separate_files: bool = False
+
+    def plot(self, sample):
+        """
+        Plots an image from the dataset.
+
+        Args:
+            sample (dict): 
+                A dictionary containing the tile to plot. The 'image' key should have a tensor of shape (C, H, W).
+
+        Returns:
+            matplotlib.figure.Figure:
+                A figure containing the plotted image.
+        """
+        # Reorder and rescale the image
+        image = sample['image'].permute(1, 2, 0)
+        image = image.byte().numpy()
+
+        # Plot the image
+        fig, ax = plt.subplots()
+        ax.imshow(image)
+        return fig
+
+class CustomVectorDataset(VectorDataset):
+    """
+    Custom dataset class for vector data which act as labels for the raster data. This class extends the `VectorDataset` from `torchgeo`.
+    """
+
+    def __getitem__(self, query: BoundingBox) -> dict[str, Any]:
+        """Retrieve image/mask and metadata indexed by query.
+           This function is largely based on the `__getitem__` method from torchgeo's `VectorDataset`, with custom modifications for this implementation.
+
+        Args:
+            query: (minx, maxx, miny, maxy, mint, maxt) coordinates to index
+
+        Returns:
+            sample of image/mask and metadata at that index
+
+        Raises:
+            IndexError: if query is not found in the index
+        """
+        hits = self.index.intersection(tuple(query), objects=True)
+        filepaths = [hit.object for hit in hits]
+
+        if not filepaths:
+            raise IndexError(
+                f"query: {query} not found in index with bounds: {self.bounds}"
+            )
+
+        shapes = []
+        for filepath in filepaths:
+            with fiona.open(filepath) as src:
+                # We need to know the bounding box of the query in the source CRS
+                (minx, maxx), (miny, maxy) = fiona.transform.transform(
+                    self.crs.to_dict(),
+                    src.crs,
+                    [query.minx, query.maxx],
+                    [query.miny, query.maxy],
+                )
+
+                # Filter geometries to those that intersect with the bounding box
+                for feature in src.filter(bbox=(minx, miny, maxx, maxy)):
+                    # Warp geometries to requested CRS
+                    shape = fiona.transform.transform_geom(
+                        src.crs, self.crs.to_dict(), feature["geometry"]
+                    )
+                    label = self.get_label(feature)
+                    shapes.append((shape, label))
+
+        # Rasterize geometries
+        width = (query.maxx - query.minx) / self.res
+        height = (query.maxy - query.miny) / self.res
+        transform = rasterio.transform.from_bounds(
+            query.minx, query.miny, query.maxx, query.maxy, width, height
+        )
+        if shapes:
+            masks = rasterio.features.rasterize(
+                shapes, out_shape=(round(height), round(width)), transform=transform
+            )
+        else:
+            # If no features are found in this query, return an empty mask
+            # with the default fill value and dtype used by rasterize
+            masks = np.zeros((round(height), round(width)), dtype=np.uint8)
+
+        # Use array_to_tensor since rasterize may return uint16/uint32 arrays.
+        masks = array_to_tensor(masks)
+
+        masks = masks.to(self.dtype)
+
+        # Beginning of additions made to this function
+
+        # Invert the transform to convert geo coordinates to pixel values
+        inverse_transform = ~transform
+
+        # Convert `fiona` type shapes to `shapely` shape objects for easier manipulation
+        shapely_shapes = [(shapely.geometry.shape(sh), i) for sh, i in shapes]
+
+        # Apply the inverse transform to each shapely shape, converting geo coordinates to pixel coordinates
+        pixel_transformed_shapes = [
+            (affine_transform(sh, inverse_transform.to_shapely()), i)
+            for sh, i in shapely_shapes
+        ]
+
+        # Add 'shapes' containing polygons and corresponding ID values
+        sample = {
+            "mask": masks,
+            "crs": self.crs,
+            "bounds": query,
+            "shapes": pixel_transformed_shapes,
+        }
+
+        if self.transforms is not None:
+            sample = self.transforms(sample)
+
+        return sample
--- tree_detection_framework/preprocessing/preprocessing.py
+++ tree_detection_framework/preprocessing/preprocessing.py
@@ -1,10 +1,23 @@
+import json
+import logging
+import random
 from typing import List, Optional
 
+import matplotlib.pyplot as plt
 import pyproj
+import rasterio
 import shapely
+import torch
 from torch.utils.data import DataLoader
-
+from torchgeo.datasets import IntersectionDataset, stack_samples, unbind_samples
+from torchgeo.samplers import GridGeoSampler, Units
+from torchvision.transforms import ToPILImage
 from tree_detection_framework.constants import ARRAY_TYPE, BOUNDARY_TYPE, PATH_TYPE
+from tree_detection_framework.preprocessing.derived_geodatasets import (
+    CustomRasterDataset,
+    CustomVectorDataset,
+)
+from tree_detection_framework.utils.geospatial import get_projected_CRS
 
 
 def create_spatial_split(
@@ -76,17 +89,101 @@ def create_dataloader(
             A dataloader containing tiles from the raster data and optionally corresponding labels
             from the vector data.
     """
-    raise NotImplementedError()
+    # Stores image data
+    raster_dataset = CustomRasterDataset(
+        paths=raster_folder_path, res=output_resolution
+    )
+
+    # Stores label data
+    vector_dataset = (
+        CustomVectorDataset(
+            paths=vector_label_folder_path,
+            res=output_resolution,
+            label_name=vector_label_attribute,
+        )
+        if vector_label_folder_path is not None
+        else None
+    )
+
+    units = Units.CRS if use_units_meters == True else Units.PIXELS
+    logging.info(f"Units = {units}")
+
+    if use_units_meters and raster_dataset.crs.is_geographic:
+        # Reproject the dataset to a meters-based CRS
+        logging.info("Projecting to meters-based CRS...")
+        lat, lon = raster_dataset.bounds[2], raster_dataset.bounds[0]
+
+        # Return a new projected CRS value with meters units
+        projected_crs = get_projected_CRS(lat, lon)
+
+        # Type conversion to rasterio.crs
+        projected_crs = rasterio.crs.CRS.from_wkt(projected_crs.to_wkt())
+
+        # Recreating the raster and vector dataset objects with the new CRS value
+        raster_dataset = CustomRasterDataset(
+            paths=raster_folder_path, crs=projected_crs
+        )
+        vector_dataset = (
+            CustomVectorDataset(
+                paths=vector_label_folder_path,
+                crs=projected_crs,
+                label_name=vector_label_attribute,
+            )
+            if vector_label_folder_path is not None
+            else None
+        )
+
+    # Create an intersection dataset that combines raster and label data if given. Otherwise, proceed with just raster_dataset.
+    final_dataset = (
+        IntersectionDataset(raster_dataset, vector_dataset)
+        if vector_label_folder_path is not None
+        else raster_dataset
+    )
+
+    if chip_overlap_percentage:
+        # Calculate `chip_stride` if `chip_overlap_percentage` is provided
+        chip_stride = chip_size * (1 - chip_overlap_percentage / 100.0)
+        logging.info(f"Calculated stride based on overlap: {chip_stride}")
+
+    elif chip_stride is None:
+        raise ValueError(
+            "Either 'chip_size' or 'chip_overlap_percentage' must be provided."
+        )
+
+    logging.info(f"Stride = {chip_stride}")
+
+    # GridGeoSampler to get contiguous tiles
+    sampler = GridGeoSampler(
+        final_dataset, size=chip_size, stride=chip_stride, units=units
+    )
+    dataloader = DataLoader(final_dataset, sampler=sampler, collate_fn=stack_samples)
+
+    return dataloader
 
 
 def visualize_dataloader(dataloader: DataLoader, n_tiles: int):
-    """Show samples from the dataloader
+    """Show samples from the dataloader.
 
     Args:
-        dataloader (DataLoader): The dataloader to visualize
-        n_tiles (int): The number of randomly-sampled tiles to show
+        dataloader (DataLoader): The dataloader to visualize.
+        n_tiles (int): The number of randomly-sampled tiles to show.
     """
-    raise NotImplementedError()
+    # Get a random sample of `n_tiles` index values to visualize
+    tile_indices = random.sample(range(len(dataloader.sampler)), n_tiles)
+
+    # Get a list of all tile bounds from the sampler
+    list_of_bboxes = list(dataloader.sampler)
+
+    for i in tile_indices:
+        sample_bbox = list_of_bboxes[i]
+
+        # Get the referenced sample from the dataloader
+        sample = dataloader.dataset[sample_bbox]
+
+        # Plot the sample image. `dataloader.dataset.datasets[0]` selects the raster_dataset.
+        dataloader.dataset.datasets[0].plot(sample)
+        plt.axis("off")
+        plt.show()
 
 
 def save_dataloader_contents(
@@ -107,6 +204,40 @@ def save_dataloader_contents(
             `random_sample`. If unset, all tiles will be saved. Defaults to None.
         random_sample: (bool, optional):
             If `n_tiles` is set, should the tiles be randomly sampled rather than taken from the
-            beginning of the dataloader. Defaults to True.
+            beginning of the dataloader. Defaults to False.
     """
-    raise NotImplementedError()
+    # Creates save directory if it doesn't exist
+    save_folder.mkdir(parents=True, exist_ok=True)
+
+    transform_to_pil = ToPILImage()
+    for i, batch in enumerate(dataloader):
+        sample = unbind_samples(batch)[0]
+
+        image = sample["image"]
+        image_tensor = torch.clamp(image / 255.0, min=0, max=1)
+        pil_image = transform_to_pil(image_tensor)
+        pil_image.save(save_folder / f"tile_{i}.png")
+
+        # Prepare tile metadata
+        metadata = {
+            "crs": sample["crs"].to_string(),
+            "bounds": list(sample["bounds"]),
+        }
+
+        # if vector dataset is part of the dataloader (i.e., includes 2 datasets), save crown metadata as well
+        if len(dataloader.dataset.datasets) == 2:
+            # Extract shapes (polygons and tree IDs)
+            shapes = sample["shapes"]
+
+            crowns = [
+                {"ID": tree_id, "crown": polygon.wkt} for polygon, tree_id in shapes
+            ]
+
+            # Add crowns to the metadata
+            metadata["crowns"] = crowns
+
+        # Save tile metadata to a json file
+        with open(save_folder / f"tile_{i}.json", "w") as f:
+            json.dump(metadata, f, indent=4)
+
+    logging.info(f"Saved {i + 1} tiles to {save_folder}")
