dd639b2f4fc970ac497e29d76787626678193be3
David Russell <djrussell@ucdavis.edu>
2024-10-21 18:15:07 +0000

    Add NMS for single region

--- tree_detection_framework/postprocessing/postprocessing.py
+++ tree_detection_framework/postprocessing/postprocessing.py
@@ -1,20 +1,46 @@
+from typing import Optional
+
+from lsnms import nms
+
 from tree_detection_framework.detection.region_detections import (
     RegionDetections,
     RegionDetectionsSet,
 )
 
 
-def single_region_NMS(detections: RegionDetections) -> RegionDetections:
+def single_region_NMS(
+    detections: RegionDetections,
+    iou_theshold: float = 0.5,
+    confidence_column: str = "score",
+) -> RegionDetections:
     """Run non-max suppresion on predictions from a single region.
 
     Args:
-        detections (RegionDetections): Detections from a single region to run NMS on.
+        detections (RegionDetections):
+            Detections from a single region to run NMS on.
+        iou_threshold (float, optional):
+            What intersection over union value to consider an overlapping detection. Defaults to 0.5.
+        confidence_column (str, optional):
+            Which column in the dataframe to use as a confidence for NMS. Defaults to "score"
 
     Returns:
         RegionDetections:
             NMS-suppressed set of detections
     """
-    raise NotImplementedError()
+    # Extract the geodataframe for the detections
+    detections_df = detections.get_detections()
+
+    # Get the axis-aligned bounds of each shape
+    boxes = detections_df.bounds.to_numpy()
+    # Extract the score
+    confidences = detections_df[confidence_column].to_numpy()
+
+    # Run NMS
+    keep = nms(boxes, confidences, iou_threshold=iou_theshold)
+    # Extract the detections that were kept
+    subset_region_detections = detections.subset_detections(keep)
+
+    return subset_region_detections
 
 
 def multi_region_NMS(detections: RegionDetectionsSet) -> RegionDetections:
