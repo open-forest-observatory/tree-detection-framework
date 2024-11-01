48effa867d0a0c1f8dc7195fb7dce3b229074cbd
David Russell <djrussell@ucdavis.edu>
2024-11-01 14:58:49 +0000

    Add utility for mask to shapely

--- tree_detection_framework/utils/geometric.py
+++ tree_detection_framework/utils/geometric.py
@@ -1,4 +1,6 @@
 import numpy as np
+import shapely
+from contourpy import contour_generator
 
 
 def get_shapely_transform_from_matrix(matrix_transform: np.ndarray):
@@ -19,3 +21,37 @@ def get_shapely_transform_from_matrix(matrix_transform: np.ndarray):
         matrix_transform[1, 2],
     ]
     return shapely_transform
+
+
+def mask_to_shapely(mask: np.ndarray) -> shapely.MultiPolygon:
+    """Convert a binary mask to a shapely polygon representing the positive regions
+
+    Args:
+        mask (np.ndarary):
+            A (n, m) array where positive values are > 0.5 and negative values are < 0.5
+
+    Returns:
+        shapely.MultiPolygon:
+            A multipolygon representing the positive regions. Holes and multiple disconnected
+            componets are properly handled.
+    """
+    # This generally follows the example here:
+    # https://contourpy.readthedocs.io/en/v1.3.0/user_guide/external/shapely.html#filled-contours-to-shapely
+
+    # Extract the contours and create a filled contour for the regions above 0.5
+    filled = contour_generator(z=mask, fill_type="ChunkCombinedOffsetOffset").filled(
+        0.5, np.inf
+    )
+
+    # Create a polygon for each of the disconected regions, called chunks in contourpy
+    # This iterates over the elements in three lists, the points, offsets, and outer offsets
+    chunk_polygons = [
+        shapely.from_ragged_array(
+            shapely.GeometryType.POLYGON, points, (offsets, outer_offsets)
+        )
+        for points, offsets, outer_offsets in zip(*filled)
+    ]
+    # Union these regions to get a single multipolygon
+    multipolygon = shapely.unary_union(chunk_polygons)
+
+    return multipolygon
