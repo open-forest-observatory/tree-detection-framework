5aa268f73d8e55d8f3672a2d960cc9914b9012c5
amrithasp02 <amrithasp02@users.noreply.github.com>
2024-11-05 18:56:19 +0000

    Apply black formatting changes

--- tree_detection_framework/entrypoints/generate_predictions.py
+++ tree_detection_framework/entrypoints/generate_predictions.py
@@ -36,9 +36,9 @@ def generate_predictions(
         output_CRS=output_CRS,
         batch_size=batch_size,
     )
-    
+
     # Setup the specified tree detection model
-    if tree_detection_model == 'deepforest':
+    if tree_detection_model == "deepforest":
 
         # Setup the parameters dictionary
         param_dict = {
@@ -57,28 +57,31 @@ def generate_predictions(
     if view_predictions_plot is True:
         outputs.plot()
 
+
 def parse_args() -> argparse.Namespace:
-    description = ("This script generates tree detections for a given raster dataset. First, it creates a dataloader "
-                   + "with the tiled data and provides the images as input to the selected tree detection model. ")
+    description = (
+        "This script generates tree detections for a given raster dataset. First, it creates a dataloader "
+        + "with the tiled data and provides the images as input to the selected tree detection model. "
+    )
     parser = argparse.ArgumentParser(description=description)
 
-    parser.add_argument('--raster-folder-path', required=True)
-    parser.add_argument('--chip-size', type=float, required=True)
-    parser.add_argument('--tree-detection-model', type=str, required=True)
-    parser.add_argument('--chip-stride', type=float)
-    parser.add_argument('--chip-overlap-percentage', type=float)
-    parser.add_argument('--use-units-meters', action='store_true')
-    parser.add_argument('--region-of-interest')
-    parser.add_argument('--output-resolution', type=float)
-    parser.add_argument('--output-CRS')
-    parser.add_argument('--save-folder')
-    parser.add_argument('--view-predictions-plot', action='store_true')
-    parser.add_argument('--batch-size', type=int, default=1)
+    parser.add_argument("--raster-folder-path", required=True)
+    parser.add_argument("--chip-size", type=float, required=True)
+    parser.add_argument("--tree-detection-model", type=str, required=True)
+    parser.add_argument("--chip-stride", type=float)
+    parser.add_argument("--chip-overlap-percentage", type=float)
+    parser.add_argument("--use-units-meters", action="store_true")
+    parser.add_argument("--region-of-interest")
+    parser.add_argument("--output-resolution", type=float)
+    parser.add_argument("--output-CRS")
+    parser.add_argument("--save-folder")
+    parser.add_argument("--view-predictions-plot", action="store_true")
+    parser.add_argument("--batch-size", type=int, default=1)
 
     args = parser.parse_args()
     return args
 
+
 if __name__ == "__main__":
     args = parse_args()
     generate_predictions(**args.__dict__)
-    
