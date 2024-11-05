0e38ddfcbc922f0488e6928d595f7f3e40657e12
Amritha <amrithasp02@gmail.com>
2024-11-05 18:40:05 +0000

    add parse_args to the script

--- tree_detection_framework/entrypoints/generate_predictions.py
+++ tree_detection_framework/entrypoints/generate_predictions.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 import pyproj
 
-from tree_detection_framework.constants import ARRAY_TYPE, BOUNDARY_TYPE, PATH_TYPE
+from tree_detection_framework.constants import BOUNDARY_TYPE, PATH_TYPE
 from tree_detection_framework.detection.detector import DeepForestDetector
 from tree_detection_framework.detection.models import DeepForestModule
 
@@ -12,19 +12,19 @@ from tree_detection_framework.preprocessing.preprocessing import create_dataload
 def generate_predictions(
     raster_folder_path: PATH_TYPE,
     chip_size: float,
+    tree_detection_model: str,
     chip_stride: Optional[float] = None,
     chip_overlap_percentage: float = None,
     use_units_meters: bool = False,
     region_of_interest: Optional[BOUNDARY_TYPE] = None,
     output_resolution: Optional[float] = None,
     output_CRS: Optional[pyproj.CRS] = None,
-    tree_detection_model: Optional[str] = None,
     save_folder: Optional[PATH_TYPE] = None,
     view_predictions_plot: bool = False,
     batch_size: int = 1,
 ):
 
-    # Create the dataloader by passing folder path to raster data and optionally a path to the vector data folder.
+    # Create the dataloader by passing folder path to raster data.
     dataloader = create_dataloader(
         raster_folder_path=raster_folder_path,
         chip_size=chip_size,
@@ -36,7 +36,8 @@ def generate_predictions(
         output_CRS=output_CRS,
         batch_size=batch_size,
     )
-
+    
+    # Setup the specified tree detection model
     if tree_detection_model == 'deepforest':
 
         # Setup the parameters dictionary
@@ -56,5 +57,28 @@ def generate_predictions(
     if view_predictions_plot is True:
         outputs.plot()
 
-    
+def parse_args() -> argparse.Namespace:
+    description = ("This script generates tree detections for a given raster dataset. First, it creates a dataloader "
+                   + "with the tiled data and provides the images as input to the selected tree detection model. ")
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument('--raster-folder-path', required=True)
+    parser.add_argument('--chip-size', type=float, required=True)
+    parser.add_argument('--tree-detection-model', type=str, required=True)
+    parser.add_argument('--chip-stride', type=float)
+    parser.add_argument('--chip-overlap-percentage', type=float)
+    parser.add_argument('--use-units-meters', action='store_true')
+    parser.add_argument('--region-of-interest')
+    parser.add_argument('--output-resolution', type=float)
+    parser.add_argument('--output-CRS')
+    parser.add_argument('--save-folder')
+    parser.add_argument('--view-predictions-plot', action='store_true')
+    parser.add_argument('--batch-size', type=int, default=1)
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    args = parse_args()
+    generate_predictions(**args.__dict__)
     
