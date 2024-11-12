2dde9c612749cf59cba0678d949f30aa39c0fd46
Amritha <amrithasp02@gmail.com>
2024-11-12 22:18:01 +0000

    setup_cfg in detectree2module

--- tree_detection_framework/detection/models.py
+++ tree_detection_framework/detection/models.py
@@ -1,5 +1,5 @@
 from typing import Any, Dict, List, Optional
-
+from pathlib import Path
 import lightning
 import torch
 import torchvision
@@ -9,6 +9,9 @@ from torchvision.models.detection.retinanet import (
     RetinaNet,
     RetinaNet_ResNet50_FPN_Weights,
 )
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2 import model_zoo
 
 from tree_detection_framework.utils.detection import use_release_df
 
@@ -139,3 +142,80 @@ class DeepForestModule(lightning.LightningModule):
         # TODO: Return 'optimizer', 'lr_scheduler', 'monitor' when validation data is set
 
         return optimizer
+
+class Detectree2Module(lightning.LightningModule):
+
+    def __init__(self, param_dict: Dict[str, Any]):
+        super().__init__()
+        self.param_dict = param_dict
+        self.setup_cfg(param_dict)
+
+    def setup_cfg(
+        base_model: str = "COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml",
+        trains=("trees_train",),
+        tests=("trees_val",),
+        update_model=None,
+        workers=2,
+        ims_per_batch=2,
+        gamma=0.1,
+        backbone_freeze=3,
+        warm_iter=120,
+        momentum=0.9,
+        batch_size_per_im=1024,
+        base_lr=0.0003389,
+        weight_decay=0.001,
+        max_iter=1000,
+        num_classes=1,
+        eval_period=100,
+        out_dir="./train_outputs",
+        resize=True,
+    ):
+        """Set up config object # noqa: D417.
+
+        Args:
+            base_model: base pre-trained model from detectron2 model_zoo
+            trains: names of registered data to use for training
+            tests: names of registered data to use for evaluating models
+            update_model: updated pre-trained model from detectree2 model_garden
+            workers: number of workers for dataloader
+            ims_per_batch: number of images per batch
+            gamma: gamma for learning rate scheduler
+            backbone_freeze: backbone layer to freeze
+            warm_iter: number of iterations for warmup
+            momentum: momentum for optimizer
+            batch_size_per_im: batch size per image
+            base_lr: base learning rate
+            weight_decay: weight decay for optimizer
+            max_iter: maximum number of iterations
+            num_classes: number of classes
+            eval_period: number of iterations between evaluations
+            out_dir: directory to save outputs
+        """
+        cfg = get_cfg()
+        cfg.merge_from_file(model_zoo.get_config_file(base_model))
+        cfg.DATASETS.TRAIN = trains
+        cfg.DATASETS.TEST = tests
+        cfg.DATALOADER.NUM_WORKERS = workers
+        cfg.SOLVER.IMS_PER_BATCH = ims_per_batch
+        cfg.SOLVER.GAMMA = gamma
+        cfg.MODEL.BACKBONE.FREEZE_AT = backbone_freeze
+        cfg.SOLVER.WARMUP_ITERS = warm_iter
+        cfg.SOLVER.MOMENTUM = momentum
+        cfg.MODEL.RPN.BATCH_SIZE_PER_IMAGE = batch_size_per_im
+        cfg.SOLVER.WEIGHT_DECAY = weight_decay
+        cfg.SOLVER.BASE_LR = base_lr
+        cfg.OUTPUT_DIR = out_dir
+        Path(cfg.OUTPUT_DIR).mkdir(parents=True, exist_ok=True)
+        if update_model is not None:
+            cfg.MODEL.WEIGHTS = update_model
+        else:
+            cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(base_model)
+
+        cfg.SOLVER.IMS_PER_BATCH = ims_per_batch
+        cfg.SOLVER.BASE_LR = base_lr
+        cfg.SOLVER.MAX_ITER = max_iter
+        cfg.MODEL.ROI_HEADS.NUM_CLASSES = num_classes
+        cfg.TEST.EVAL_PERIOD = eval_period
+        cfg.RESIZE = resize
+        cfg.INPUT.MIN_SIZE_TRAIN = 1000
+        return cfg
