622b6f1a14fd99d8d1ad5b591f17d6154e2fc32a
Amritha <amrithasp02@gmail.com>
2024-11-20 20:23:36 +0000

    changes made to adapt pytorch lightning for detectree2

--- tree_detection_framework/detection/models.py
+++ tree_detection_framework/detection/models.py
@@ -1,12 +1,37 @@
+import logging
+import os
+import time
+import weakref
+from collections import OrderedDict
 from pathlib import Path
 from typing import Any, Dict, List, Optional
 
 import lightning
 import torch
 import torchvision
+import detectron2.utils.comm as comm
 from detectron2 import model_zoo
 from detectron2.config import get_cfg
-from detectron2.engine import DefaultPredictor
+from detectron2.engine import DefaultPredictor, DefaultTrainer
+from detectron2.modeling import build_model
+from detectron2.solver import build_lr_scheduler, build_optimizer
+from detectron2.utils.events import EventStorage
+from detectron2.utils.logger import setup_logger
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.data import build_detection_test_loader, build_detection_train_loader
+from detectron2.engine import (
+    DefaultTrainer,
+    SimpleTrainer,
+    default_argument_parser,
+    default_setup,
+    default_writers,
+    hooks,
+)
+from detectron2.evaluation import print_csv_format
+from detectron2.evaluation.testing import flatten_results_dict
+
+from tools.train_net import build_evaluator
+
 from torch import Tensor, optim
 from torchvision.models.detection.retinanet import (
     AnchorGenerator,
@@ -15,7 +40,12 @@ from torchvision.models.detection.retinanet import (
 )
 
 from tree_detection_framework.utils.detection import use_release_df
+from tree_detection_framework.preprocessing.derived_geodatasets import CustomDataModule
 
+from torchvision.models.detection import maskrcnn_resnet50_fpn
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("detectron2")
 
 class RetinaNetModel:
     """A backbone class for DeepForest"""
@@ -143,92 +173,188 @@ class DeepForestModule(lightning.LightningModule):
         # TODO: Return 'optimizer', 'lr_scheduler', 'monitor' when validation data is set
 
         return optimizer
-
-
-class Detectree2Module:
-    def __init__(self, param_dict: Optional[Dict[str, Any]] = None):
+    
+class Detectree2Module(lightning.LightningModule):
+    def __init__(self):
         super().__init__()
-        # If param_dict is not provided, ensure it is an empty dictionary
-        self.param_dict = param_dict or {}
-        self.cfg = self.setup_cfg(**self.param_dict)
+        self.weights = torch.load("/ofo-share/repos-amritha/detectree2-code/230103_randresize_full.pth")
+        self.model = maskrcnn_resnet50_fpn(weights=self.weights)
 
-    def setup_cfg(
-        self,
-        base_model: str = "COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml",
-        trains=("trees_train",),
-        tests=("trees_val",),
-        update_model=None,
-        workers=2,
-        ims_per_batch=2,
-        gamma=0.1,
-        backbone_freeze=3,
-        warm_iter=120,
-        momentum=0.9,
-        batch_size_per_im=1024,
-        base_lr=0.0003389,
-        weight_decay=0.001,
-        max_iter=1000,
-        num_classes=1,
-        eval_period=100,
-        out_dir="./train_outputs",
-        resize=True,
-    ):
-        """Set up config object.
+    def forward(
+        self, images: List[Tensor], targets: Optional[List[Dict[str, Tensor]]] = None
+    ) -> Dict[str, Tensor]:
+        """Calls the model's forward method.
         Args:
-            base_model: base pre-trained model from detectron2 model_zoo
-            trains: names of registered data to use for training
-            tests: names of registered data to use for evaluating models
-            update_model: updated pre-trained model from detectree2 model_garden
-            workers: number of workers for dataloader
-            ims_per_batch: number of images per batch
-            gamma: gamma for learning rate scheduler
-            backbone_freeze: backbone layer to freeze
-            warm_iter: number of iterations for warmup
-            momentum: momentum for optimizer
-            batch_size_per_im: batch size per image
-            base_lr: base learning rate
-            weight_decay: weight decay for optimizer
-            max_iter: maximum number of iterations
-            num_classes: number of classes
-            eval_period: number of iterations between evaluations
-            out_dir: directory to save outputs
-            resize: whether to resize input images
+            images (list[Tensor]): Images to be processed
+            targets (list[Dict[Tensor]]): Ground-truth boxes present in the image (optional)
+
+        Returns:
+            result (list[BoxList] or dict[Tensor]):
+                The output from the model.
+                During training, it returns a dict[Tensor] which contains the losses.
         """
-        # Initialize configuration
-        cfg = get_cfg()
-        cfg.merge_from_file(model_zoo.get_config_file(base_model))
+        # Move the data to the same device as the model
+        images = images.to(self.device)
+        return self.model.forward(images, targets=targets)  # Model specific forward
 
-        # Assign values, prioritizing those in param_dict
-        cfg.DATASETS.TRAIN = self.param_dict.get("trains", trains)
-        cfg.DATASETS.TEST = self.param_dict.get("tests", tests)
-        cfg.DATALOADER.NUM_WORKERS = self.param_dict.get("workers", workers)
-        cfg.SOLVER.IMS_PER_BATCH = self.param_dict.get("ims_per_batch", ims_per_batch)
-        cfg.SOLVER.GAMMA = self.param_dict.get("gamma", gamma)
-        cfg.MODEL.BACKBONE.FREEZE_AT = self.param_dict.get(
-            "backbone_freeze", backbone_freeze
+    def training_step(self, batch):
+        # Ensure model is in train mode
+        self.model.train()
+        device = next(self.model.parameters()).device
+
+        # Image is expected to be a list of tensors, each of shape [C, H, W] in 0-1 range.
+        image_batch = (batch["image"][:, :3, :, :] / 255.0).to(device)
+        image_batch_list = [image for image in image_batch]
+
+        # To store every image's target - a dictionary containing `boxes` and `labels`
+        targets = []
+        for tile in batch["bounding_boxes"]:
+            # Convert from list to FloatTensor[N, 4]
+            boxes_tensor = torch.tensor(tile, dtype=torch.float32).to(device)
+            # Need to remove boxes that go out-of-bounds. Has negative values.
+            valid_mask = (boxes_tensor >= 0).all(dim=1)
+            filtered_boxes_tensor = boxes_tensor[valid_mask]
+            # Create a label tensor. Single class for now.
+            class_labels = torch.zeros(
+                filtered_boxes_tensor.shape[0], dtype=torch.int64
+            ).to(device)
+            # Dictionary for the tile
+            d = {"boxes": filtered_boxes_tensor, "labels": class_labels}
+            targets.append(d)
+
+        loss_dict = self.forward(image_batch_list, targets=targets)
+
+        final_loss = sum([loss for loss in loss_dict.values()])
+        print("loss: ", final_loss)
+        return final_loss
+
+    def configure_optimizers(self):
+        # similar to the one in deepforest
+        optimizer = optim.SGD(
+            self.model.parameters(), lr=self.param_dict["train"]["lr"], momentum=0.9
         )
-        cfg.SOLVER.WARMUP_ITERS = self.param_dict.get("warm_iter", warm_iter)
-        cfg.SOLVER.MOMENTUM = self.param_dict.get("momentum", momentum)
-        cfg.MODEL.RPN.BATCH_SIZE_PER_IMAGE = self.param_dict.get(
-            "batch_size_per_im", batch_size_per_im
-        )
-        cfg.SOLVER.WEIGHT_DECAY = self.param_dict.get("weight_decay", weight_decay)
-        cfg.SOLVER.BASE_LR = self.param_dict.get("base_lr", base_lr)
-        cfg.OUTPUT_DIR = self.param_dict.get("out_dir", out_dir)
-        cfg.SOLVER.MAX_ITER = self.param_dict.get("max_iter", max_iter)
-        cfg.MODEL.ROI_HEADS.NUM_CLASSES = self.param_dict.get(
-            "num_classes", num_classes
-        )
-        cfg.TEST.EVAL_PERIOD = self.param_dict.get("eval_period", eval_period)
-        cfg.RESIZE = self.param_dict.get("resize", resize)
-        cfg.INPUT.MIN_SIZE_TRAIN = 1000
 
-        # Create output directory if it doesn't exist
-        Path(cfg.OUTPUT_DIR).mkdir(parents=True, exist_ok=True)
+        # TODO: Setup lr_scheduler
+        # TODO: Return 'optimizer', 'lr_scheduler', 'monitor' when validation data is set
 
-        # Set model weights
-        cfg.MODEL.WEIGHTS = self.param_dict.get(
-            "update_model", update_model
-        ) or model_zoo.get_checkpoint_url(base_model)
+        return optimizer
 
-        return cfg
+
+############# TRIAL 2 ####################################
+# class Detectree2Module(lightning.LightningModule):
+#     def __init__(self, cfg):
+#         super().__init__()
+#         self.cfg = cfg
+#         self.model = build_model(self.cfg)
+
+#     def training_step(self, batch):
+#          image_batch = batch["image"][:, :3, :, :]
+#          # image_batch_list = [image for image in image_batch]
+#          loss_dict = self.model([image_batch])
+#          print("LOSS: ", loss_dict)
+#          return sum(loss_dict.values())
+    
+#     def validation_step(self, batch, batch_idx: int, dataloader_idx: int = 0) -> None:
+#         if not isinstance(batch, List):
+#             batch = [batch]
+#         outputs = self.model(batch)
+#         self._evaluators[dataloader_idx].process(batch, outputs)
+
+#     def configure_optimizers(self):
+#         optimizer = build_optimizer(self.cfg, self.model)
+#         self._best_param_group_id = hooks.LRScheduler.get_best_param_group_id(optimizer)
+#         scheduler = build_lr_scheduler(self.cfg, optimizer)
+#         return [optimizer], [{"scheduler": scheduler, "interval": "step"}]
+
+
+
+
+
+############ TRIAL 1 ####################################
+# class Detectree2Module(lightning.LightningModule):
+#     def __init__(self, cfg):
+#         super().__init__()
+#         self.cfg = cfg
+
+#         if not logger.isEnabledFor(logging.INFO):  # setup_logger is not called for d2
+#             setup_logger()
+#         self.cfg = DefaultTrainer.auto_scale_workers(self.cfg, comm.get_world_size())
+#         self.storage: EventStorage = None
+#         self.model = build_model(self.cfg)
+
+#         self.start_iter = 0
+#         self.max_iter = self.cfg.SOLVER.MAX_ITER
+
+
+#     def training_step(self, batch, batch_idx):
+#         if self.storage is None:
+#             self.storage = EventStorage(0)
+#             self.storage.__enter__()
+#             # self.iteration_timer.trainer = weakref.proxy(self)
+#             # self.iteration_timer.before_step()
+#             self.writers = (
+#                 default_writers(self.cfg.OUTPUT_DIR, self.max_iter)
+#                 if comm.is_main_process()
+#                 else {}
+#             )
+
+#         loss_dict = self.model(batch)
+#         # SimpleTrainer.write_metrics(loss_dict, data_time)
+#         print("LOSS: ", loss_dict)
+
+#         opt = self.optimizers()
+#         self.storage.put_scalar(
+#             "lr",
+#             opt.param_groups[self._best_param_group_id]["lr"],
+#             smoothing_hint=False,
+#         )
+#         # self.iteration_timer.after_step()
+#         self.storage.step()
+#         # A little odd to put before step here, but it's the best way to get a proper timing
+#         # self.iteration_timer.before_step()
+
+#         if self.storage.iter % 20 == 0:
+#             for writer in self.writers:
+#                 writer.write()
+#         return sum(loss_dict.values())
+
+#     def validation_step(self, batch, batch_idx: int, dataloader_idx: int = 0) -> None:
+#         if not isinstance(batch, List):
+#             batch = [batch]
+#         outputs = self.model(batch)
+#         self._evaluators[dataloader_idx].process(batch, outputs)
+
+#     def configure_optimizers(self):
+#         optimizer = build_optimizer(self.cfg, self.model)
+#         self._best_param_group_id = hooks.LRScheduler.get_best_param_group_id(optimizer)
+#         scheduler = build_lr_scheduler(self.cfg, optimizer)
+#         return [optimizer], [{"scheduler": scheduler, "interval": "step"}]
+
+    
+# def train(cfg, data_module):
+#     trainer_params = {
+#         # training loop is bounded by max steps, use a large max_epochs to make
+#         # sure max_steps is met first
+#         "max_epochs": 10**8,
+#         "max_steps": cfg.SOLVER.MAX_ITER,
+#         "val_check_interval": 1, # cfg.TEST.EVAL_PERIOD if cfg.TEST.EVAL_PERIOD > 0 else 10**8,
+#         "num_nodes": 1,
+#         # "gpus": 1,
+#         "num_sanity_val_steps": 0,
+#     }
+#     if cfg.SOLVER.AMP.ENABLED:
+#         trainer_params["precision"] = 16
+
+
+#     trainer = lightning.Trainer(**trainer_params)
+
+#     module = Detectree2Module(cfg)
+#     logger.info("Running training")
+#     trainer.fit(module, data_module)
+
+#     # if args.eval_only:
+#     #     logger.info("Running inference")
+#     #     trainer.validate(module, data_module)
+#     # else:
+#     #     logger.info("Running training")
+#     #     trainer.fit(module, data_module)
