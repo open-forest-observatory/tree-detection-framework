48215fddc61ddb91d511e2c99bbdce221249fa3c
amrithasp02 <amrithasp02@users.noreply.github.com>
2025-04-16 17:08:03 +0000

    Apply black formatting changes

--- tree_detection_framework/detection/SAM2_detector.py
+++ tree_detection_framework/detection/SAM2_detector.py
@@ -34,18 +34,18 @@ class SAMV2Detector(Detector):
             model_cfg, sam2_checkpoint, device=self.device, apply_postprocessing=False
         )
         self.mask_generator = SAM2AutomaticMaskGenerator(
-                                model=self.sam2,
-                                points_per_side=64,
-                                points_per_batch=128,
-                                pred_iou_thresh=0.7,
-                                stability_score_thresh=0.92,
-                                stability_score_offset=0.7,
-                                crop_n_layers=1,
-                                box_nms_thresh=0.7,
-                                crop_n_points_downscale_factor=2,
-                                min_mask_region_area=25.0,
-                                use_m2m=True,
-                            )
+            model=self.sam2,
+            points_per_side=64,
+            points_per_batch=128,
+            pred_iou_thresh=0.7,
+            stability_score_thresh=0.92,
+            stability_score_offset=0.7,
+            crop_n_layers=1,
+            box_nms_thresh=0.7,
+            crop_n_points_downscale_factor=2,
+            min_mask_region_area=25.0,
+            use_m2m=True,
+        )
 
     def call_predict(self, batch):
         """
