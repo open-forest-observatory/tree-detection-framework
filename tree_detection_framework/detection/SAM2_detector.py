6671207fcd8f7b0422e352a8897235f47c183279
Amritha <amrithasp02@gmail.com>
2026-08-05 21:10:24 +0000

    change defaults for SAM2

--- tree_detection_framework/detection/SAM2_detector.py
+++ tree_detection_framework/detection/SAM2_detector.py
@@ -32,11 +32,11 @@ class SAMV2Detector(Detector):
         score_metric="predicted_iou",
         points_per_side=64,
         points_per_batch=128,
-        pred_iou_thresh=0.7,
-        stability_score_thresh=0.92,
+        pred_iou_thresh=0.5,  # Changed from optimal value 0.7
+        stability_score_thresh=0.7,  # Changed from optimal value 0.92
         stability_score_offset=0.7,
         crop_n_layers=1,
-        box_nms_thresh=0.7,
+        box_nms_thresh=1,  # Disabled SAM2's box NMS. TDF will use its own NMS during postprocessing. Previously it was set to 0.7.
         crop_n_points_downscale_factor=2,
         min_mask_region_area=25.0,
         use_m2m=True,
